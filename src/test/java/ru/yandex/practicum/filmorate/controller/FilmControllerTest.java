95b3039b53760a87c95b6e3c6766717be5a3b5b4
semzz <semzzcs@gmail.com>
2025-04-10 14:48:28 +0300

    fix: FilmControllerTest

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -27,14 +27,14 @@ class FilmControllerTest {
                 .name("name")
                 .description("Desc")
                 .releaseDate(LocalDate.of(2022, 12, 12))
-                .duration(Duration.ofMinutes(144))
+                .duration(144)
                 .build();
         Film film2 = Film.builder()
                 .id(2L)
                 .name("name2")
                 .description("Desc2")
                 .releaseDate(LocalDate.of(2012, 2, 12))
-                .duration(Duration.ofMinutes(113))
+                .duration(113)
                 .build();
         filmController.addFilm(film1);
         filmController.addFilm(film2);
@@ -49,7 +49,7 @@ class FilmControllerTest {
                 .name("name")
                 .description("Desc")
                 .releaseDate(LocalDate.of(1700, 12, 12))
-                .duration(Duration.ofMinutes(144))
+                .duration(144)
                 .build();
 
         assertThrows(InvalidFilmInputException.class, () -> filmController.addFilm(filmInvalidDate));
@@ -62,7 +62,7 @@ class FilmControllerTest {
                 .name("name")
                 .description("Desc")
                 .releaseDate(LocalDate.of(2000, 12, 12))
-                .duration(Duration.ofMinutes(144))
+                .duration(144)
                 .build();
         filmController.addFilm(film);
         film.setReleaseDate(LocalDate.of(1700, 10, 10));
@@ -72,7 +72,7 @@ class FilmControllerTest {
                 .name("name")
                 .description("Desc")
                 .releaseDate(LocalDate.of(2000, 12, 12))
-                .duration(Duration.ofMinutes(144))
+                .duration(144)
                 .build();
 
         assertThrows(InvalidFilmInputException.class, () -> filmController.updateFilm(film));
