577d0d40b196f30d5386b32e16f1da3cdd67f658
semzz <semzzcs@gmail.com>
2025-05-31 14:36:36 +0300

    fix: data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,24 @@
-SET referential_integrity FALSE;
-TRUNCATE TABLE film_genres;
-TRUNCATE TABLE genres RESTART IDENTITY;
-TRUNCATE TABLE likes;
-TRUNCATE TABLE friends;
-TRUNCATE TABLE status RESTART IDENTITY;
-TRUNCATE TABLE ratings RESTART IDENTITY;
-TRUNCATE TABLE users RESTART IDENTITY;
-TRUNCATE TABLE films RESTART IDENTITY;
-SET referential_integrity TRUE;
+MERGE INTO ratings AS r
+    USING (VALUES
+               ('G'),
+               ('PG'),
+               ('PG-13'),
+               ('R'),
+               ('NC-17')
+        ) AS vals(name)
+ON r.name = vals.name
+WHEN NOT MATCHED THEN
+    INSERT (name) VALUES (vals.name);
 
-INSERT INTO ratings(name)
-VALUES ('G'),
-       ('PG'),
-       ('PG-13'),
-       ('R'),
-       ('NC-17');
-
-INSERT INTO genres (name)
-VALUES ('Комедия'),
-       ('Драма'),
-       ('Мультфильм'),
-       ('Триллер'),
-       ('Документальный'),
-       ('Боевик');
+MERGE INTO genres AS g
+    USING (VALUES
+               ('Комедия'),
+               ('Драма'),
+               ('Мультфильм'),
+               ('Триллер'),
+               ('Документальный'),
+               ('Боевик')
+        ) AS vals(name)
+ON g.name = vals.name
+WHEN NOT MATCHED THEN
+    INSERT (name) VALUES (vals.name);
