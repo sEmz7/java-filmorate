650dcd0f1c40d2439ac2398e0209002021b865e3
Nikolay Druzhinin <northernNikolay@yandex.ru>
2025-06-10 23:18:18 +0300

    fix: tests

--- src/test/java/ru/yandex/practicum/filmorate/db/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/db/FilmDbStorageTest.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.db;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
